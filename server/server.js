a8544dac801e88b34843be24646a943580055d36
SamratChakraborty <samratch@outlook.com>
2020-10-31 04:49:10 +0530

    Logger added at server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 const NodeCache = require( "node-cache" );
+const logger = require('./logger').logger;
 
 
 // Setup the express app
@@ -19,40 +20,40 @@ var clientIdMap = new NodeCache({
 // Bind socket.io to the server
 var io = require('socket.io')(server);
 io.on('connection', function(socket){
-  console.log("New Client Conmected");
+  logger.info("New Client Conmected");
   socket.on('register', function(data){
     if(data == undefined || !data instanceof String){
-      console.log(`Invalid client Id provided with registration request`);
+      logger.info(`Invalid client Id provided with registration request`);
       io.to(socket.id).emit('register', 'falied');
       return;
     }
-    console.log(`new Registration for ${data} sock Id ${socket.id}`);
+    logger.info(`new Registration for ${data} sock Id ${socket.id}`);
     clientIdMap.set(data, socket.id);
-    //console.log('Client Id Map', clientIdMap);
-    console.log(`${data} successfully registered with id ${socket.id}`);
+    //logger.info('Client Id Map', clientIdMap);
+    logger.info(`${data} successfully registered with id ${socket.id}`);
     io.to(socket.id).emit('register','successful');
   });
 
   socket.on('input', (data) => {
     try { 
-      console.log('input data 1', data);
+      logger.info('input data 1', data);
       data = JSON.parse(data);
-      console.log(`input data`, data);
+      logger.info(`input data`, data);
       transmit('input', data, socket.id);
     } catch(ex){
-      console.error("Error at input event", ex);
+      logger.error("Error at input event", ex);
       io.to(socket.id).emit('input','Error at input event');
     }
   });
 
   socket.on('output', (data) => {
     try {
-      console.log('output data 1', data);
+      logger.info('output data 1', data);
       data = JSON.parse(data);
-      console.log(`output data`, data);
+      logger.info(`output data`, data);
       transmit('output', data, socket.id);
     } catch(ex){
-      console.error('Error at output event', ex);
+      logger.error('Error at output event', ex);
       io.to(socket.id).emit('output','Error at output event');
     }
   })
@@ -64,11 +65,11 @@ io.on('connection', function(socket){
       clientIdMap.set(message.from, id);
       if(!clientIdMap.has(message.to))
         throw new Error(`${message.to} Reciver offline`);
-      console.log(`Sending ${event} to sock id ${clientIdMap.get(message.to)} body ${message.body}`);
+      logger.info(`Sending ${event} to sock id ${clientIdMap.get(message.to)} body ${message.body}`);
       io.to(clientIdMap.get(message.to)).emit(event, JSON.stringify(message));
       io.to(clientIdMap.get(message.from)).emit(event, 'successful');
     } catch (ex){
-      console.error('Error while transmitting message', ex);
+      logger.error('Error while transmitting message', ex);
       io.to(socket.id).emit(event, ex.message);
     }
   };
