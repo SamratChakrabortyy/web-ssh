bfbbdec9acd655adfb454d49aa03c965b2e86406
SamratChakraborty <samratch@outlook.com>
2020-10-31 04:55:02 +0530

    Logger config Change

---
+++ server/logger.js
@@ -0,0 +1,19 @@
+const log4js = require("log4js");
+const log4js_extend = require("log4js-extend");
+const logger_config = require("./logger_config.json");
+
+
+log4js.configure(logger_config);
+
+log4js_extend(log4js, {
+  path: null,
+  format: "[@name @file:@line:@column]"
+});
+
+const logger = log4js.getLogger('[web-ssh]');
+const httpLogger = log4js.getLogger('[HTTP]');
+logger.info('Logger Enabled');
+
+exports.logger = logger;
+exports.httpLogger = httpLogger;
+
